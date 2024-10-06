cc468cbf6fd29a5f567b88aa183e52fb47e29dbb
Hennadii Zhurbenko <hennadii.zhurbenko@uklon.com.ua>
2024-10-06 14:34:56 +0300

    Migration to ANGULAR 18, add Three.JS example to main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,17 @@
+import { NgtCanvas } from 'angular-three';
+import { Experience } from './experience/experience.component';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
-
-
 @Component({
-  standalone: true,
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  imports: [RouterModule, HeaderComponent, FooterComponent],
+    standalone: true,
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    imports: [RouterModule, HeaderComponent, FooterComponent, NgtCanvas],
 })
 export class AppComponent {
-  title = 'my-angular-app';
+    sceneGraph = Experience;
+    title = 'my-angular-app';
 }
