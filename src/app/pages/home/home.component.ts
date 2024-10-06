03f183bb986246f2be0f72cca52b928f62874ff2
Ded0Ak <davidkagricenok@gmail.com>
2024-10-06 20:21:20 +0300

    main

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 import { NgtCanvas } from 'angular-three';
 import { Experience } from '../../experience/experience.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NgtCanvas],
+  imports: [NgtCanvas, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
   sceneGraph = Experience;
-  
 }
